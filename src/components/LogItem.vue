0ab131ff0aac18a2430ba1c4f877887d826726d1
xnousnow <xnou.snow@gmail.com>
2023-03-11 20:52:18 +0900

    :construction: Add logged item UI

---
+++ src/components/LogItem.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LogItem',
+  props: {
+    year: {
+      type: Number,
+      required: true
+    },
+    month: {
+      type: Number,
+      required: true
+    },
+    day: {
+      type: Number,
+      required: true
+    },
+    body: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<template>
+  <ul class="flex max-w-[50rem] gap-3">
+    <div>
+      <div class="flex font-semibold">
+        <span class="relative">{{ month }}</span>
+        <span class="text-lg">/</span>
+        <span class="self-end">{{ day }}</span>
+      </div>
+    </div>
+    <p>{{ body }}</p>
+  </ul>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,23 @@
+<script lang="ts">
+import LogItem from '../components/LogItem.vue'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    LogItem
+  }
+})
+</script>
+
 <template>
-  <span class="text-xl">Hello, world!</span>
+  <ul class="flex flex-col items-center justify-center gap-5 p-10 grow">
+    <LogItem
+      v-for="i in 5"
+      :key="i"
+      :year="2023"
+      :month="6 - i"
+      :day="1 + i"
+      body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel."
+    />
+  </ul>
 </template>
