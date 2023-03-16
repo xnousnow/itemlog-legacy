65757f785e2a31557750ea20e71cb9e05bc52099
xnousnow <xnou.snow@gmail.com>
2023-03-16 21:36:27 +0900

    :fire: Remove default test items

--- src/App.vue
+++ src/App.vue
@@ -20,77 +20,7 @@ interface Item {
 export default defineComponent({
   data() {
     return {
-      items: {
-        ab8cfd: {
-          name: 'Item 1',
-          body: {
-            0: {
-              year: 2023,
-              month: 1,
-              day: 1,
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel.'
-            },
-            1: {
-              year: 2023,
-              month: 2,
-              day: 13,
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel.'
-            },
-            3: {
-              year: 2023,
-              month: 2,
-              day: 15,
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel.'
-            },
-            4: {
-              year: 2023,
-              month: 2,
-              day: 27,
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel.'
-            }
-          }
-        },
-        fdbe97: {
-          name: 'Item 2',
-          body: {
-            0: {
-              year: 2023,
-              month: 1,
-              day: 3,
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel.'
-            },
-            1: {
-              year: 2023,
-              month: 3,
-              day: 9,
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel.'
-            }
-          }
-        },
-        ab472c: {
-          name: 'Item 3',
-          body: {
-            1: {
-              year: 2023,
-              month: 1,
-              day: 1,
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel.'
-            },
-            3: {
-              year: 2023,
-              month: 1,
-              day: 24,
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel.'
-            },
-            4: {
-              year: 2023,
-              month: 2,
-              day: 3,
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sit amet dui porta, accumsan turpis sed, rhoncus urna. Aliquam porta consectetur sapien, vel sagittis turpis molestie vel.'
-            }
-          }
-        }
-      } as Record<string, Item>
+      items: {} as Record<string, Item>
     }
   },
   methods: {
