0829058c70e09f656969ea82f2b4ff4456ab9a25
xnousnow <xnou.snow@gmail.com>
2023-03-18 20:59:16 +0900

    :wrench: Setup `vue-i18n` package

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createI18n } from 'vue-i18n'
 
 import './assets/main.css'
 
@@ -10,5 +11,14 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+const i18n = createI18n({
+  fallbackLocale: 'en'
+})
+
+const lang = navigator.language.split('-')[0]
+i18n.global.locale = lang
+
+app.use(i18n)
 
 app.mount('#app')
